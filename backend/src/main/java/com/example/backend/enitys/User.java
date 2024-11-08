0248f7c45dbd5dc22ba733e09af3ff0fc7fc138a
Oleksandr Kozoriz <oleksandr@ip-154-150.wifi.tuke.sk>
2024-11-08 21:51:18 +0100

    User template

--- backend/src/main/java/com/example/backend/enitys/User.java
+++ backend/src/main/java/com/example/backend/enitys/User.java
@@ -1,4 +1,20 @@
 package com.example.backend.enitys;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
 public class User {
+    @Id
+    @Generated
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+
 }
